b9e763cff84b61904fb4d4b02177dfc0b15c6a6c
bhargav232 <bhargavmanavadariya1998@gmail.com>
2024-04-23 14:09:55 -0400

    successfully created login routes with jwttoken

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,11 @@
 import express from "express"
-import registerController from "../controllers/registerController.js"
+import { loginController, registerController } from "../controllers/authControllers.js"
 
 const router = express.Router()
 
 router.post("/register", registerController)
+
+router.post('/login', loginController)
+
 
 export default router;
--- server.js
+++ server.js
@@ -7,22 +7,26 @@ import authRoutes from "./routes/authRoute.js"
 
 const app = express();
 dotenv.config();
+
 // middlewares
 app.use(morgan('dev'));
-app.use(express.json())
-app.use("/api/v1/auth", authRoutes)
+app.use(express.json());
+
+// Use authRoutes with prefix
+app.use("/api/v1/auth", authRoutes);
+
+// Your non-prefixed routes
+app.get("/", (req, res) => {
+    res.send('I am up and running');
+});
+
+
 
 connectDB();
 
-app.get("/", (req, res)=>{
-    res.send('I am up and running');
-})
-
-
 const PORT = process.env.PORT || 8080;
-const dev = process.env.DEV_MODE
+const dev = process.env.DEV_MODE;
 
-app.listen(PORT, ()=>{
-    console.log(`I am running on port ${PORT} in ${dev} mode`.bgCyan.white)
-}
-)
+app.listen(PORT, () => {
+    console.log(`I am running on port ${PORT} in ${dev} mode`.bgCyan.white);
+});
